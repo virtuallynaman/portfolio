71852f62a3f1e5d65d83f122cdfcf6363c7158a1
virtuallynaman <jhanaman786@gmail.com>
2025-08-28 13:53:05 +0530

    Add responsiveness using tailwind css

--- frontend/src/components/ScrollableContent.jsx
+++ frontend/src/components/ScrollableContent.jsx
@@ -1,12 +1,12 @@
 import { FaGithub } from 'react-icons/fa';
-import { LinkIcon, ArrowUpRightIcon } from '@heroicons/react/24/outline';
+import { ArrowUpRightIcon } from '@heroicons/react/24/outline';
 
 function ScrollableContent() {
     return (
-        <div className="scrollable-content main-scroll-animation" id='main'>
-            <section id="about" className="section section-scroll-animation about">
-                <h2 className="section-title about">About</h2>
-                <p className="about-section-content">
+        <div className="lg:mr-6 gap-12 content-center flex flex-col main-scroll-animation" id='main'> {/* mr, min-width for other screens */}
+            <section id="about" className="section-scroll-animation">
+                <h2 className="text-3xl">About</h2>
+                <p className="text-xl">
                     I'm a full-stack web developer with a strong focus on building modern, and user-focused applications. I specialize in the PERN stack (PostgreSQL, Express.js, React, Node.js), and I'm constantly learning new tools and technologies to improve my craft. I enjoy solving real-world problems through clean code and a thoughtful design.
                     <br />
                     <br />
@@ -14,11 +14,11 @@ function ScrollableContent() {
                 </p>
             </section>
 
-            <section id="skills" className="section">
-                <h2 className="section-title">Skills</h2>
-                <div className="skill-section-container">
-                    <div className="skill-category">
-                        <div className="skill-category-title">
+            <section>
+                <h2 className="text-3xl my-7">Skills</h2>
+                <div className="flex justify-between mb-8">
+                    <div>
+                        <div className="text-xl uppercase font-bold mb-3.5">
                             Languages
                         </div>
                         <ul>
@@ -31,8 +31,8 @@ function ScrollableContent() {
                         </ul>
                     </div>
                     <div>
-                        <div className="skill-category">
-                            <div className="skill-category-title">
+                        <div>
+                            <div className="text-xl uppercase font-bold mb-3.5">
                                 Frameworks
                             </div>
                             <ul>
@@ -41,8 +41,8 @@ function ScrollableContent() {
                                 <li className="skill-name">Express</li>
                             </ul>
                         </div>
-                        <div className="skill-category">
-                            <div className="skill-category-title database-title">
+                        <div>
+                            <div className="text-xl uppercase font-bold mt-8 mb-1">
                                 Database
                             </div>
                             <ul>
@@ -50,8 +50,8 @@ function ScrollableContent() {
                             </ul>
                         </div>
                     </div>
-                    <div className="skill-category">
-                        <div className="skill-category-title">
+                    <div>
+                        <div className="text-xl uppercase font-bold mb-3.5">
                             Tools
                         </div>
                         <ul>
@@ -66,48 +66,48 @@ function ScrollableContent() {
                 </div>
             </section>
 
-            <section id="projects" className="section section-scroll-animation project-section">
-                <h2 className="section-title">Projects</h2>
-                <div className="project-section-content">
-                    <div className="project section-scroll-animation">
-                        <div className="project-title">
+            <section id="projects" className="mb-20 section-scroll-animation">
+                <h2 className="text-3xl my-7">Projects</h2>
+                <div className="flex flex-col gap-24">
+                    <div className="section-scroll-animation">
+                        <div className="text-2xl text-[var(--accent)] font-semibold flex items-center gap-1 cursor-pointer hover:text-[var(--text)] transition-colors duration-400 ease-in-out group">
                             <a href="https://starward-budgets.vercel.app/" target='_blank' rel="noopener noreferrer" className='project-link'>Starward Budgets</a>
-                            <ArrowUpRightIcon className='project-arrow-icon' />
+                            <ArrowUpRightIcon className='h-6 stroke-3 transition-all duration-200 ease-in-out group-hover:translate-x-1 group-hover:-translate-y-1 group-hover:text-[var(--text)]' />
                         </div>
 
-                        <img src="/starward-budgets.png" className="project-demo" alt="Starward budgets project demo" />
+                        <img src="/starward-budgets.png" className="my-6" alt="Starward budgets project demo" />
 
-                        <div className="project-description">
-                            <ul>
+                        <div className="text-xl project-description">
+                            <ul className='flex flex-col gap-1.5'>
                                 <li>Developed a budgeting app that lets users create budgets, log expenses, assign colors to label them, and track total, spent, and remaining balances with live progress bars (React, Express, Node).</li>
                                 <li>Designed relational tables for budgets & expenses using PostgreSQL and wrote optimized SQL queries for real-time aggregates.</li>
                                 <li>Implemented JWT-based signup/login to secure personal finance data and deployed the app on Vercel.</li>
                             </ul>
                         </div>
 
-                        <a className="link project-github-link" href="https://github.com/virtuallynaman/starward-budgets" target="_blank" rel="noopener noreferrer">
-                            <FaGithub className='react-icon' /> GITHUB <LinkIcon className='github-link-icon' />
+                        <a className="link flex gap-1.5 mt-6" href="https://github.com/virtuallynaman/starward-budgets" target="_blank" rel="noopener noreferrer">
+                            <FaGithub className='react-icon' /> GITHUB
                         </a>
                     </div>
 
-                    <div className="project section-scroll-animation">
-                        <div className="project-title">
-                            <a href="https://starward-notes.vercel.app/" target='_blank' rel="noopener noreferrer" className='project-link'>Starward Notes</a> <ArrowUpRightIcon className='project-arrow-icon' />
+                    <div className="section-scroll-animation">
+                        <div className="text-2xl text-[var(--accent)] font-semibold flex items-center gap-1 cursor-pointer hover:text-[var(--text)] transition-colors duration-400 ease-in-out group">
+                            <a href="https://starward-notes.vercel.app/" target='_blank' rel="noopener noreferrer" className='project-link'>Starward Notes</a> <ArrowUpRightIcon className='h-6 stroke-3 transition-all duration-200 ease-in-out group-hover:translate-x-1 group-hover:-translate-y-1 group-hover:text-[var(--text)]' />
                         </div>
 
-                        <img src="/starward-notes.png" className="project-demo" alt="Starward notes project demo" />
+                        <img src="/starward-notes.png" className="my-6 mx-auto" alt="Starward notes project demo" />
 
-                        <div className="project-description">
-                            <ul>
+                        <div className="text-xl project-description">
+                            <ul className='flex flex-col gap-1.5'>
                                 <li>Created a web app for taking and organizing notes with autosave, pin/archive/trash, background‑color labels, and quick text search (React, Express, Node).</li>
                                 <li>Designed a PostgreSQL schema to persist user notes and user accounts efficiently.</li>
-                                <li>Implemented JWT‑based user authentication for signup/login security.</li>
-                                <li>Wrote vanilla CSS for desktop UI (mobile layout in progress).</li>
+                                <li>Implemented JWT‑based user authentication for signup, login security.</li>
+                                <li>Wrote vanilla CSS for desktop UI; mobile layout in progress.</li>
                             </ul>
                         </div>
 
-                        <a className="link project-github-link" href="https://github.com/virtuallynaman/starward-notes" target="_blank" rel="noopener noreferrer">
-                            <FaGithub className='react-icon' /> GITHUB <LinkIcon className='github-link-icon' />
+                        <a className="link flex gap-1.5 mt-6" href="https://github.com/virtuallynaman/starward-notes" target="_blank" rel="noopener noreferrer">
+                            <FaGithub className='react-icon' /> GITHUB
                         </a>
                     </div>
                 </div>
--- frontend/src/components/StickySidebar.jsx
+++ frontend/src/components/StickySidebar.jsx
@@ -3,23 +3,35 @@ import { MdEmail } from "react-icons/md";
 
 function StickySidebar() {
     return (
-        <div className="sticky-sidebar section-scroll-animation">
+        <div className="lg:sticky top-0 flex flex-col h-dvh gap-18 lg:mx-14 section-scroll-animation">
             <div>
-                <h1 className="name sidebar-name">Naman Jha</h1>
-                <h3 className="sidebar-title">Web Developer</h3>
-                <p className="sidebar-tagline">I turn ideas into web applications using the PERN stack and beyond.</p>
+                <h1 className="mt-52 text-5xl font-semibold">Naman Jha</h1>
+                <h3 className="my-3 font-semibold text-xl">Web Developer</h3>
+                <p className="w-72">I turn ideas into web applications using the PERN stack and beyond.</p>
             </div>
 
-            <div className="resume-container">
+            <div className="flex items-center w-fit group">
                 <a href="https://flowcv.com/resume/ti457sanpo" className="resume-link section-scroll-animation" target="_blank" rel="noopener noreferrer">
-                    View my Résumé <FaArrowRight className='resume-arrow-icon' />
+                    View my Résumé <FaArrowRight className='h-6 transition-transform duration-200 ease-in-out group-hover:translate-x-1.5' />
                 </a>
             </div>
 
-            <ul className="socials-container">
-                <li><a className="link social-links" href="https://github.com/virtuallynaman" target="_blank" rel="noopener noreferrer"><FaGithub className="react-icon" />GITHUB</a></li>
-                <li><a className="link social-links" href="https://www.linkedin.com/in/namanjha/" target="_blank" rel="noopener noreferrer"><FaLinkedin className="react-icon" />LINKEDIN</a></li>
-                <li><a className="link social-links" href="mailto:namanjhacs@gmail.com" target="_blank" rel="noopener noreferrer"><MdEmail className="react-icon" />EMAIL</a></li>
+            <ul className="flex lg:flex-col gap-2">
+                <li>
+                    <a className="link flex gap-1.5" href="https://github.com/virtuallynaman" target="_blank" rel="noopener noreferrer">
+                        <FaGithub className="react-icon" />GITHUB
+                    </a>
+                </li>
+                <li>
+                    <a className="link flex gap-1.5" href="https://www.linkedin.com/in/namanjha/" target="_blank" rel="noopener noreferrer">
+                        <FaLinkedin className="react-icon" />LINKEDIN
+                    </a>
+                </li>
+                <li>
+                    <a className="link flex gap-1.5" href="mailto:namanjhacs@gmail.com" target="_blank" rel="noopener noreferrer">
+                        <MdEmail className="react-icon" />EMAIL
+                    </a>
+                </li>
             </ul>
         </div>
     )
